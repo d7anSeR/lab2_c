c12d2c224b99c9505db9a4241ada12c8b3266d5a
daniya_kuspanova <kyspanova@icloud.com>
2023-04-14 20:45:59 +0400

    add function selection sort

--- lab2_c/lab2_c.cpp
+++ lab2_c/lab2_c.cpp
@@ -1,20 +1,59 @@
-﻿// lab2_c.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <locale.h>
 #include <iostream>
+#include <conio.h>
+#include <vector>
+#include <algorithm>
+#include <chrono>
+#include <iostream>
+#include<vector>
+using namespace std;
+struct stats {
+	size_t comparison_count = 0;
+	size_t copy_count = 0;
+};
+//class iter
+//{
+//private:
+//	int value;
+//public:
+//	iter(int value):value(value) {}
+//
+//};
 
-int main()
+void selection_sort(std::vector<int>:: iterator iter1, std::vector<int>::iterator iter2)
 {
-    std::cout << "Hello World!\n";
+	while(iter1 != iter2)
+	{
+		auto small{ &iter };
+		auto iter (iter1 + 1);
+		while (iter != iter2)
+		{
+			if (iter < small)
+				small = iter;
+			++iter;
+		}
+		std::swap(iter1, small);
+		++iter1;
+	}
+
 }
+int main() 
+{
+	std::vector<int> numbers{ 10, 20, 30, 40 };
+	auto iter1{ numbers.begin() };
+	auto iter2{ numbers.end() };  
+	selection_sort(iter1, iter2)
+	while (iter != numbers.end())
+	{
+		std::cout << *iter << std::endl;
+		++iter;           
+	}
+	for (auto start{ numbers.begin() }; start != numbers.end(); start++)
+	{
+		std::cout << *start << std::endl;
+	}
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+}
