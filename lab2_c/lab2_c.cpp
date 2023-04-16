18f5fde5d36f070dc017647d23a9d328c73e1c8b
daniya_kuspanova <kyspanova@icloud.com>
2023-04-16 17:54:30 +0400

    with grief in half, a third sorting - head sort has been added

--- lab2_c/lab2_c.cpp
+++ lab2_c/lab2_c.cpp
@@ -13,75 +13,105 @@ struct stats
 	size_t comparison_count = 0;
 	size_t copy_count = 0;
 };
+void print_mass(std::vector<int>::iterator iter1, std::vector<int>::iterator iter2)
+{
+	std::vector<int>::iterator t = iter1;
+	while (t <= iter2)
+	{
+		cout << *t << endl;
+		++t;
+	}
+}
+void heapify(std::vector<int>::iterator it1, std::vector<int>::iterator it2, int i) 
+{
+	int n = it2 - it1;
+	int largest = i;
+	int l = 2 * i + 1;
+	int r = 2 * i + 2;
+	if (l < n && *(it1 + l) > *(it1 + largest)) 
+		largest = l;
+	if (r < n && *(it1 + r) > *(it1 + largest)) 
+		largest = r;
+	if (largest != i) 
+	{
+		std::swap(*(it1 + i), *(it1 + largest));
+		heapify(it1, it2, largest);
+	}
+}
+stats heap_sort(std::vector<int>::iterator iter1, std::vector<int>::iterator iter2) 
+{
+	stats flag;
+	int n = iter2 - iter1;
+	std::vector<int>::iterator it = iter1;
+	for (int i = n / 2 - 1; i >= 0; i--) 
+	{
+		heapify(it, iter2, i);
+	}
+	for (int i = n - 1; i >= 0; i--) 
+	{
+		std::swap(*(it), *(it + i));
+		heapify(it, it + i, 0);
+	}
+	print_mass(iter1, iter2);
+}
+
 stats shell_sort(std::vector<int>::iterator iter1, std::vector<int>::iterator iter2)
 {
 	stats flag;
-	int n = 0;
-	std::vector<int>::iterator temp = iter1;
-	while (temp <= iter2)
+	int n = iter2 - iter1 + 1;
+	for (int step = n / 2; step > 0; step /= 2) 
 	{
-		n++;
-		++temp;
-	}
-	int i = 0, j = 0, step = 0;
-	for (step = n / 2; step > 0; step /= 2)
-	{
-		for (i = step; i < n; i++)
+		std::vector<int>::iterator temp1 = iter1, temp2 = iter1, temp3 = iter1;
+		for (temp1 += step; temp1 <= iter2; temp1++)
 		{
-			int tmp = *(iter1 + i - 1);
-			for (j = i; j >= step; j -= step)
+			int temp = *(temp1);
+			flag.copy_count++;
+			for (temp2 = temp1; temp2 >= temp3 + step && *(temp2 - step) > temp; temp2 -= step) 
 			{
-				if (tmp < *(iter1 + j - step))
-					iter1 + j = iter1 + j - step;
-				else
-					break;
+				*(temp2) = *(temp2 - step);
 			}
-			*(iter1 + j) = tmp;
+			*(temp2) = temp;
 		}
 	}
-	std::vector<int>::iterator tmp3 = iter1;
-	while (tmp3 <= iter2)
-	{
-		cout << *tmp3 << endl;
-		++tmp3;
-	}
+	print_mass(iter1, iter2);
 	return flag;
 }
 
 stats selection_sort(std::vector<int>:: iterator iter1, std::vector<int>::iterator iter2)
 {
 	stats flag;
-	while(iter1 <= iter2)
+	std::vector<int>::iterator tmp = iter1;
+	while(tmp <= iter2)
 	{
-		flag.comparison_count += 2;
-		std::vector<int>::iterator small_ = iter1;
-		if (iter1 != iter2)
+		std::vector<int>::iterator small_ = tmp;
+		if (tmp != iter2)
 		{
-			std::vector<int>::iterator iter_ = iter1 + 1;
+			std::vector<int>::iterator iter_ = tmp + 1;
 			while (iter_ <= iter2)
 			{
-				flag.comparison_count += 2;
 				if (*iter_ < *small_)
 				{
 					small_ = iter_;
 				}
-				++iter_;
+				iter_++;
 			}
-			flag.comparison_count++;
 		}
-		iter_swap(iter1, small_);
+		iter_swap(tmp, small_);
 		flag.copy_count++;
-		cout << *iter1 << endl;
-		++iter1;
+		tmp++;
 	}
-	flag.comparison_count++;
+	print_mass(iter1, iter2);
 	return flag;
 }
 int main() 
 {
-	std::vector<int> numbers = { 15, 0, 1, 18, 94, 22, 43, 1010, 32, 14, 16, 19};
+	std::vector<int> numbers = { 15, 0, 1, 18, 94, 22, 43, 1010, 32, 17, 16, 19, 2, 1019};
 	auto iter1{ numbers.begin() };
-	auto iter2{ numbers.end() - 1};  
-	/*selection_sort(iter1, iter2);*/
+	auto iter2{ numbers.end() - 1};
+	selection_sort(iter1, iter2);
+	cout << endl;
 	shell_sort(iter1, iter2);
+	cout << endl;
+	heap_sort(iter1, iter2);
+	cout << endl;
 }
